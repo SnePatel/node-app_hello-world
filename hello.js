cf631335615eb630b28fc105e503d5be89434d42
SnePatel <46272377+SnePatel@users.noreply.github.com>
2021-07-30 16:10:32 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('welcome to jenkins pipeline!!\n');
+  res.end('welcome to webelight solution!!\n');
 });
 
 server.listen(port, hostname, () => {
