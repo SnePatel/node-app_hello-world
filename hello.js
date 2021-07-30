67a81a3e8a2a83d80433aea137322d258906160e
SnePatel <46272377+SnePatel@users.noreply.github.com>
2021-07-30 10:56:19 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Welcome to Webelight Solution!\n');
+  res.end('Welcome to Webelight Solution!!\n');
 });
 
 server.listen(port, hostname, () => {
